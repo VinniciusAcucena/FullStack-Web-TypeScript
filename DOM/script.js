1d9eba0ad72be408c0abcae677ba3c864fbc8e41
VinniciusAcucena <acucena39@gmail.com>
2025-12-26 14:33:05 -0300

    complete element and attribute selection, add img folder

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,4 @@
+//Selecionando elementos individuais
 const objGetId = document.getElementById('destaque')
 
 const objQuerySelector = document.querySelector('h1')
@@ -13,6 +14,7 @@ function alterar() {
     console.log(objQuery)
 }
 
+//Selecionando múltiplo elementos
 const objTagName = document.getElementsByTagName('h1')
 for (i = 0; i < objTagName.length; i++) {
     console.log(objTagName[i].textContent)
@@ -31,3 +33,55 @@ console.log(objName)
 
 const objQueryAll = document.querySelectorAll('li')
 console.log(objQueryAll.item(1).textContent)
+
+//Selecionando formulário
+function preencher(){
+    document.cadastro.nome.value = "Vinnicius"
+    document.cadastro.email.value = "vinnicius.acucena@capoviagens.com.br"
+    document.cadastro.sexo.value = "masculino"
+
+}
+
+//navegando por elementos
+const obj = document.querySelector('div#itens')
+console.log(obj.parentNode)
+console.log(obj.childNodes)
+console.log(obj.firstChild)
+console.log(obj.previousSibling)
+console.log(obj.nextSibling)
+console.log(obj.children)
+console.log(obj.firstElementChild)
+console.log(obj.previousElementSibling)
+
+//selecionando atributos
+
+const img = document.querySelector('img')
+console.log(img.getAttribute('src'))
+console.log(img.src)
+console.log(img.width)
+
+const link = document.querySelector('#link')
+console.log(link.href)
+
+function mudar() {
+    img.src = "img/grafico.png"
+
+}
+
+//Atributos personalizados
+const personalizado = document.getElementById('itens')
+personalizado.setAttribute('quantidade', '10')
+console.log(personalizado.getAttribute('quantidade'))
+personalizado.dataset.status = "finalizado"
+console.log(personalizado.getAttribute('data-status'))
+
+//selecionando classes
+const classe = document.querySelector('#span')
+const listaClasses = classe.classList
+
+console.log(listaClasses)
+
+function executar() {
+    listaClasses.add('teste')
+    listaClasses.remove('texto')
+}
