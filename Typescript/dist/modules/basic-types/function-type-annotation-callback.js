5818d2550a2b496b1fe8c71ff276c142a7de3546
VinniciusAcucena <acucena39@gmail.com>
2026-01-02 12:24:56 -0300

    Add function types files

---
+++ Typescript/dist/modules/basic-types/function-type-annotation-callback.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getPrincipalAddress = exports.calculateTotal = void 0;
+const shoppingCart = {
+    cartItems: [
+        { id: 1, price: 100 },
+        { id: 2, price: 150 },
+        { id: 3, price: 200 }
+    ]
+};
+const addresses = [
+    { cep: '00000-416', default: true },
+    { cep: '00000-400', default: false },
+    { cep: '00000-001', default: true }
+];
+const customer = {
+    addresses: addresses
+};
+const applyDiscount = (total, discount) => {
+    return total - (total * discount);
+};
+let calculateTotal = function (shoppingCart, applyDiscount) {
+    const total = shoppingCart.cartItems.reduce((acc, item) => acc + item.price, 0);
+    return applyDiscount(total, 0.10);
+};
+exports.calculateTotal = calculateTotal;
+let getPrincipalAddress;
+exports.getPrincipalAddress = getPrincipalAddress = (customer) => {
+    return customer.addresses.find(addresses => addresses.default);
+};
+const total = calculateTotal(shoppingCart, applyDiscount);
+console.log(total);
+const principalAddres = getPrincipalAddress(customer);
+console.log(principalAddres);
---
+++ Typescript/dist/modules/basic-types/function-type-void.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculateTotal = calculateTotal;
+const shoppingCart = {
+    cartItems: [
+        { id: 1, price: 100 },
+        { id: 2, price: 150 },
+        { id: 3, price: 200 }
+    ]
+};
+function calculateTotal(shoppingCart) {
+    const total = shoppingCart.cartItems.reduce((acc, item) => acc + item.price, 0);
+    console.log(`Total do valor ${total.toFixed(2)}`);
+}
+calculateTotal(shoppingCart);
