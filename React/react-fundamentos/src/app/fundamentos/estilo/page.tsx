0ce0fd6e18e629a04f3a1d075c5863e81fc93180
VinniciusAcucena <acucena39@gmail.com>
2026-01-05 17:11:53 -0300

    Add styles for pages folder

---
+++ React/react-fundamentos/src/app/fundamentos/estilo/page.tsx
@@ -0,0 +1,9 @@
+import "@/app/estilo.css"
+
+export default function Page() {
+    return (
+        <div>
+            <h1 className="titulo">Estilo</h1>
+        </div>
+    )
+}
--- React/react-fundamentos/src/app/fundamentos/propriedades/page.tsx
+++ React/react-fundamentos/src/app/fundamentos/propriedades/page.tsx
@@ -3,6 +3,7 @@ import { Acomodacao, Cabecalho, Menu } from "@/components/interface";
 export default function Page() {
     return(
         <div>
+            <h1 className="bg-blue-400">Propriedades (APP)</h1>
             <Cabecalho 
             titulo="Localização, Brasil" 
             preco="R$ 350.00"/>
