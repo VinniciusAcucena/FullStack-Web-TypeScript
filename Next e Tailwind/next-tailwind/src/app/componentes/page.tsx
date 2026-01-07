cebef1a0b3cc739c1d5e7b7866c0417236c3544a
VinniciusAcucena <acucena39@gmail.com>
2026-01-07 13:59:16 -0300

    Created components layer in globals.css

---
+++ Next e Tailwind/next-tailwind/src/app/componentes/page.tsx
@@ -0,0 +1,17 @@
+export default function Componentes() {
+    return (
+        <div className="p-2">
+
+            <button className="btn btn-sucesso">
+                Sucesso
+            </button>
+            <button className="btn btn-info">
+                Informações
+            </button>
+            <button className="btn btn-erro">
+                Error
+            </button>
+
+        </div>
+    )
+}
