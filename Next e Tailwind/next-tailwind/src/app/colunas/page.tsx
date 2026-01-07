b1eafd10fd29301ce667c0b8da776880c291117d
VinniciusAcucena <acucena39@gmail.com>
2026-01-07 14:14:19 -0300

    Add responsive columns

---
+++ Next e Tailwind/next-tailwind/src/app/colunas/page.tsx
@@ -0,0 +1,24 @@
+export default function Colunas() {
+    return (
+        <div className="p-2 columns-1 md:columns-2 lg:columns-3">
+            <div className="w-32 h-32 border bg-blue-500">
+                Div 1
+            </div>
+            <div className="w-32 h-32 border bg-blue-500">
+                Div 2
+            </div>
+            <div className="w-32 h-32 border bg-blue-500">
+                Div 3
+            </div>
+            <div className="w-32 h-32 border bg-blue-500">
+                Div 4
+            </div>
+            <div className="w-32 h-32 border bg-blue-500">
+                Div 5
+            </div>
+            <div className="w-32 h-32 border bg-blue-500">
+                Div 6
+            </div>
+        </div>
+    )
+}
