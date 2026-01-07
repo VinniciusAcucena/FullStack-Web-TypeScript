1ea9a1ec37a97386699835c74dda5fb18bd3e2ef
VinniciusAcucena <acucena39@gmail.com>
2026-01-07 13:36:58 -0300

    Praticed spacing, sizes and borders with Tailwind

---
+++ Next e Tailwind/next-tailwind/src/app/espacamentos/page.tsx
@@ -0,0 +1,23 @@
+export default function Espacamento() {
+    return (
+        <div className="m-auto">
+            <h1 className="pt-4 pl-3 pb-9 bg-blue-400">
+                Teste de espaçamentos
+            </h1>
+
+
+            <div className="bg-yellow-500 w-1/5 m-auto">
+                <h1>
+                    Teste de tamanhos
+                </h1>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non facere ut ullam vel assumenda voluptatem esse necessitatibus debitis odio? Culpa excepturi minima ab odio sequi sint recusandae nam laudantium unde?
+            </div>
+
+            <div className="border-red-600 border-4 rounded-full border-dashed">
+                <h1 className="m-0.5 p-1">
+                    Teste de bordas
+                </h1>
+            </div>
+        </div>
+    )
+}
