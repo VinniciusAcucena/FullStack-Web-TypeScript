4c425a604d3841dd805d5ee329af943cdcf92e7e
VinniciusAcucena <acucena39@gmail.com>
2026-01-30 15:51:54 -0300

    Create route handler file

---
+++ Projeto API REST/spotify-backoffice/src/app/api/route.ts
@@ -0,0 +1,27 @@
+export function GET(request: Request) {
+  return Response.json({ msg: "API método GET funcionando!" });
+}
+
+export function POST(request: Request) {
+  return Response.json({ msg: "API método POST funcionando!" });
+}
+
+export function PUT(request: Request) {
+  return Response.json({ msg: "API método PUT funcionando!" });
+}
+
+export function DELETE(request: Request) {
+  return Response.json({ msg: "API método DELETE funcionando!" });
+}
+
+export function PATCH(request: Request) {
+  return Response.json({ msg: "API método PATCH funcionando!" });
+}
+
+export function OPTIONS(request: Request) {
+  return Response.json({ msg: "API método OPTIONS funcionando!" });
+}
+
+export function HEAD(request: Request) {
+  return Response.json({ msg: "API método HEAD funcionando!" });
+}
