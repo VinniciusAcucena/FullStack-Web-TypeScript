7e3fd3dd164019212413a22d3ec524929a026358
VinniciusAcucena <acucena39@gmail.com>
2026-02-19 12:39:14 -0300

    Add IndicatorSkeleto for suspense

---
+++ Projeto API REST/spotify-backoffice/src/app/(admin)/home/componentes/IndicatorSkeleton.tsx
@@ -0,0 +1,8 @@
+export default async function Indicator() {
+  return (
+    <div className="flex flex-col items-center justify-center border-[1px] border-gray-800 text-gray-800 bg-gray-100 rounded-sm py-4 px-12">
+      <p className="h-4 w-32 bg-gray-300 rounded"></p>
+      <p className="h-4 w-32 bg-gray-300 rounded"></p>
+    </div>
+  );
+}
--- Projeto API REST/spotify-backoffice/src/app/(admin)/home/page.tsx
+++ Projeto API REST/spotify-backoffice/src/app/(admin)/home/page.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from "react";
 import Indicator from "./componentes/Indicator";
+import IndicatorSkeleton from "./componentes/IndicatorSkeleton";
 
 export default async function Page() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-800">Home</h1>
       <div className="flex gap-8">
-        <Indicator type="bands"></Indicator>
-        <Indicator type="tracks"></Indicator>
+        <Suspense fallback={<IndicatorSkeleton />}>
+          <Indicator type="bands"></Indicator>
+        </Suspense>
+        <Suspense fallback={<IndicatorSkeleton />}>
+          <Indicator type="tracks"></Indicator>
+        </Suspense>
       </div>
     </div>
   );
